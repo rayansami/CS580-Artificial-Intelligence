f4e601b6c0e3b67572cd2c51deb9b605820c78fe
Sami Uddin <rayan_sami@outlook.com>
2020-02-08 16:54:10 -0500

    Starting project for Informed Search

---
+++ 24-Puzzle-Problem-Informed-Search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24_Puzzle_Problem_Informed_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
